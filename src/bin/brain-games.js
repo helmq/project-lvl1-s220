a3949bd6f6c3bb3aa1a5fbe3361c60209a7797c3
Viktor <jarlaks52@gmail.com>
2018-01-23 08:51:55 +0900

    add eslint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
-#!/usr/bin/env node 
-import greet from "..";
+#!/usr/bin/env node
+import greet from '..';
 
 console.log('Welcome to the Brain Games!\n');
 console.log(greet());
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import readlineSync from 'readline-sync';
 const upFirstLetter = str => str[0].toUpperCase() + str.slice(1);
 
 const greet = () => {
-    const name = upFirstLetter(readlineSync.question('May I have your name ? '));
-    return `Hello, ${name}!`;
+  const name = upFirstLetter(readlineSync.question('May I have your name ? '));
+  return `Hello, ${name}!`;
 };
 
 export default greet;
