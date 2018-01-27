2f86c16cf54c12c186308b62a8795d5fc1461e6b
Viktor <jarlaks52@gmail.com>
2018-01-27 17:18:53 +0900

    fix loop with recursion

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,21 +3,23 @@ import * as helper from '../helper';
 
 const description = 'What number is missing in this progression?';
 
-const size = 10;
+const progressionSize = 10;
 
 const generateProgression = () => {
   const first = helper.generateRandom(0, 10);
-  const progression = [first];
   const step = helper.generateRandom(2, 10);
-  while (progression.length < size) {
-    progression.push(first + (step * progression.length));
-  }
-  return progression;
+  const iter = (acc) => {
+    if (acc.length === progressionSize) {
+      return acc;
+    }
+    return iter([...acc, first + (acc.length * step)]);
+  };
+  return iter([]);
 };
 
 const generator = () => {
-  const skippedIndex = helper.generateRandom(0, size - 1);
   const progression = generateProgression();
+  const skippedIndex = helper.generateRandom(0, progressionSize - 1);
   const skipped = progression[skippedIndex];
   progression[skippedIndex] = '..';
   return { question: progression.join(' '), result: `${skipped}` };
